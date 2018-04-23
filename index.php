20f44b0f71f0300b074ac679231144a79a7d34a1
jeffreydereus <99045621@mydavinci.nl>
2018-04-23 12:00:42 +0200

    Added little framework

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
+require "pages/Templates/Header.php";
+?>
 
-</body>
-</html>
+<H1>Hello world</H1>
+<?php
+require "pages/Templates/footer.php";
+?>
